92e0a8384697c9af6e4a8d478c9c7e816e58d064
trancuong3100 <cuongct989@gmail.com>
2025-12-15 18:39:08 +0700

    upload

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         //
+        'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -22,5 +23,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+         $this->registerPolicies();
+       
     }
 }
